5dd85579b2025a396fd8778497db0e718cb980ff
divyanshmehta355 <145423867+divyansh355@users.noreply.github.com>
2024-12-18 14:46:28 +0530

    ✅ Level up Bitwise Operators

--- src/NumberSystem/DecimalToBinary.java
+++ src/NumberSystem/DecimalToBinary.java
@@ -3,7 +3,44 @@ package NumberSystem;
 import java.util.Scanner;
 
 public class DecimalToBinary {
+
+    static int toDecimal(int binary){
+        int result = 0;
+        int index = 0;
+
+        while(binary != 0){
+            int lastDigit = binary % 10;
+            if (lastDigit == 1){
+                result = result + (int) Math.pow(2, index);
+            }
+            index = index + 1;
+            binary = binary / 10;
+        }
+        return result;
+    }
+
+    static String toBinary(int decimal){
+        String result = "";
+
+        while (decimal != 0){
+            int lastBit = decimal & 1;
+            result = lastBit + result;
+            decimal = decimal >> 1;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+//        String ans = toBinary(n);
+//        System.out.println(ans);
+        int ans = toDecimal(n);
+        System.out.println(ans);
+    }
+
+    static void DecimalToBinary(){
         //Decimal to Binary
         Scanner scanner = new Scanner(System.in);
 
